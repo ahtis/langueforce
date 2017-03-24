6d89a2ecba1e3cd655165d9ebd2d8451d326b595
eahtsoo <ahti.soom@ericsson.com>
2017-03-24 16:00:09 +0200

    Remove autogenerated js files

--- src/app/menu-items.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.MENU_BRAND = { href: 'home', name: 'Langueforce' };
-exports.MENU_ITEMS = [
-    { href: 'about', name: 'Ettevõttest' },
-    { href: 'services', name: 'Teenused' },
-    { href: 'prices', name: 'Hinnad' },
-    { href: 'contacts', name: 'Kontakt' }
-];
-//# sourceMappingURL=menu-items.js.map
